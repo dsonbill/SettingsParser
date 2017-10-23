4e4f256c240b5992cdd15b1924f295a153003c26
William C Donaldson <dson.bill@gmail.com>
2017-10-23 04:40:31 -0700

    Change to .NET Standard 2.0

--- SettingsParser/Properties/AssemblyInfo.cs
+++ SettingsParser/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SettingsParser")]
 [assembly: AssemblyDescription("Simple key equals value (key=value) parser for C#.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alexandre Oliveira & Christopher Andrews")]
-[assembly: AssemblyProduct("SettingsParser")]
 [assembly: AssemblyCopyright("Copyright © 2015, Alexandre Oliveira & Christopher Andrews")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ec7c9073-6112-458e-a6d4-8378e1a0064a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
